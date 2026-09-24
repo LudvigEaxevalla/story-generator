e7dfc9c1aa1045cc12d85a1f0c7a1a0301738fe8
LudvigEaxevalla <ludvigeinarsson@hotmail.com>
2026-09-24 14:15:35 +0200

    stuff

--- obj/Debug/net10.0/Picker.AssemblyInfo.cs
+++ obj/Debug/net10.0/Picker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Picker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c20760b70a4816baa48bc35309099a0f4bb1da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acf5287d2014a70198d2e38bbad3dca9fd84ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Picker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Picker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
